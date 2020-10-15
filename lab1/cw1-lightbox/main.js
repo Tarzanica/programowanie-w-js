3daf6a96b244f5caa4cf10c36dc2a7c2566a5c92
jaur001 <patrycja.bobek@microsoft.wsei.edu.pl>
2020-10-15 09:05:00 +0200

    lightbox, changing image my clicking prev/after

--- lab1/cw1-lightbox/main.js
+++ lab1/cw1-lightbox/main.js
@@ -1,32 +1,77 @@
 //pobranie referencji
 const gallery = document.querySelectorAll('.gallery img');
-
+const lightbox = document.querySelector('.lightbox');
+const galleryCount = gallery.length;
 //zapisanie się na zdarzenie click
+
 
 for (let idx = 0; idx < gallery.length; idx++){
     const img = gallery[idx];
-    img.addEventListener('click', showLightbox);
+    img.addEventListener('click',showLightboxEvent);
 }
 
-const lightbox = document.querySelector('.lightbox');
-if (lightbox.classList.contains('visible')) {
-    for (let idx = 0; idx < gallery.length; idx++) {
+
+function showLightboxEvent(ev) {
+    showLightbox(ev.target);
+}
+
+function showLightbox(target) {
+    const prevEl = target.previousElementSibling;
+    const nextEl = target.nextElementSibling;
+    const img = document.querySelector('.lightbox img');
+    const imgUrl = target.src;
+    img.src = imgUrl;
+    lightbox.classList.add('visible');
+    if(prevEl != null){
+        const prevImg = document.querySelector('.prev');
+        prevImg.children[0].href = prevEl.src;
+        console.log(prevImg);
+        prevImg.addEventListener('click', prevImage);
+        document.querySelector('.prev').style.display = "block";
+    } else {
+        document.querySelector('.prev').style.display = "none";
+    }
+
+    if(nextEl != null){
+        const nextImg = document.querySelector('.next');
+        nextImg.children[0].href = nextEl.src;
+        console.log(nextImg);
+        nextImg.addEventListener('click', nextImage);
+        document.querySelector('.next').style.display = "block";
+    } else {
+        document.querySelector('.next').style.display = "none";
+    }
+
+}
+
+function prevImage(ev){
+    for (let idx = 0; idx < gallery.length; idx++){
         const img = gallery[idx];
-        img.addEventListener('click', hideLightbox); 
+        if(img.src == ev.target.children[0].href){
+            showLightbox(img);
+            break;
+        }
     }
 }
 
-
-function showLightbox(ev) {
-    console.log(ev.target);
-    const lightbox = document.querySelector('.lightbox');
-    const img = document.querySelector('.lightbox img');
-    const imgUrl = ev.target.src;
-    img.src = imgUrl;
-    lightbox.classList.add('visible');
+function nextImage(ev){
+    for (let idx = 0; idx < gallery.length; idx++){
+        const img = gallery[idx];
+        if(img.src === ev.target.children[0].href){
+            showLightbox(img);
+            break;
+        }
+    }
 }
 
-function hideLightbox(){
-    const lightbox = document.querySelector('.lightbox');
-    lightbox.classList.remove('visible');
+//hideLightbox
+const lightBox = document.querySelector('.lightbox');
+
+lightbox.addEventListener('click', hideLightbox);
+
+function hideLightbox(e) {
+    if (e.target == e.currentTarget) {
+        return lightBox.classList.remove('visible');
+    }
 }
+s
