31a613979ac4b8ea8372adeb3c29d60bcbe21685
Tarzanica <patrycja.bobek@microsoft.wsei.edu.pl>
2020-11-18 22:47:18 +0100

    moveBall fail attempt

--- lab1/cw4-ball/main.js
+++ lab1/cw4-ball/main.js
@@ -1,21 +1,16 @@
 let holes = [];
 let speedX = 0;
 let speedY = 0;
+let x = 200;
+let y = 200;
+let ball = document.querySelector('ball');
 
-const ball =  {
-    x: 0,
-    y: 0
-}
 
 
 window.addEventListener('deviceorientation', onDeviceOrientationChange);
 
 document.querySelector('.start').addEventListener('click', onStartClick);
 // document.querySelectorAll('.restart').addEventListener('click', onRestart);
-
-
-
-
 
 
 function onStartClick() {
@@ -36,42 +31,30 @@ function onStartClick() {
     document.body.appendChild(ball);
 
 
-
-    gameInit();
 }
 
-function gameInit() {
-
-    const ballStartXPos = Math.random() * innerWidth;
-    const ballStartYPos = Math.random() * innerHeight;
-
-    ball.x = ballStartXPos;
-    ball.y = ballStartYPos;
-
-    
-    
-}
 
 function onDeviceOrientationChange(ev) {
-    speedX = ev.gamma/60;
-    speedY = ev.beta/60;
-}
+    console.log(ev);
+    speedX = ev.alfa/45;
+    speedY = ev.beta/45;
 
+}
 
 // function onRestart() {
 //     window.onStartClick()
 // }
 
 function makeHoles() {
-    for (let i = 5; i < window.innerWidth/60; i++) {
+    for (let i = 5; i < window.innerWidth/100; i++) {
         let hole = document.createElement('div');
         hole.classList.add('hole');
         let holeNumber = document.createElement('span');
         holeNumber.innerHTML = i - 5;
         hole.appendChild(holeNumber);
 
-        hole.style.left = 80 * i+Math.random() * 70 - 95 + 'px';
-        hole.style.top = Math.random() * (window.innerHeight-95) + 'px';
+        hole.style.left = 80 * i + Math.random() * 50 + 'px';
+        hole.style.top = Math.random() * (window.innerHeight - 100) + 'px';
 
         holes.push(hole);
         document.body.appendChild(hole);
@@ -81,6 +64,15 @@ function makeHoles() {
 
 
 
-function moveBall() {
-
-}
+ function moveBall() {
+//error cannot read property of null ??
+     if ( speedX + x < window.innerWidth-200 && speedX + x > 0 ) {
+         x += speedX;
+         ball.style.left = speedX + 'px';
+     }
+        
+     if ( speedY + y < window.innerWidth-200 && speedY + y > 0 ) {
+        y += speedY;
+        ball.style.left = speedY + 'px';
+    }
+ }
