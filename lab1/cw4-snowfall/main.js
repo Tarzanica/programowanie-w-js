f883e0e7dc925aa67dd1c6fb3358ad132159c0fb
Tarzanica <patrycja.bobek@microsoft.wsei.edu.pl>
2020-12-10 14:08:57 +0100

    background deleted

--- lab1/cw4-snowfall/main.js
+++ lab1/cw4-snowfall/main.js
@@ -1,9 +1,11 @@
 let canvas = document.querySelector('#snowField');
 let ctx = canvas.getContext('2d');
-let numberOfFlakes = 700;
+let numberOfFlakes = 400;
 let flakes = [];
-let w = canvas.width = window.innerWidth;
-let h = canvas.height = window.innerHeight;
+let w = window.innerWidth;
+let h = window.innerHeight;
+canvas.width = w;
+canvas.height = h;
 
 function random(min, max) {
     min = Math.ceil(min);
@@ -11,18 +13,17 @@ function random(min, max) {
     return min = Math.random() * (max - min);
 };
 
-
-function createSnowflakes() {
+function createSnowflakes() {    
     for (let i = 0; i < numberOfFlakes; i++) {
         flakes.push({
             x: Math.random() * w,
             y: Math.random() * h,
-            speedX: random(-10, 10),
-            speedY: random(5, 17),
+            speedX: random(-5, 5),
+            speedY: random(0, 10),
             radius: random(0.5, 7),
         })        
     }
-};
+}
 
 function drawSnowflakes() {
     ctx.clearRect(0, 0, w, h);
@@ -43,10 +44,13 @@ function drawSnowflakes() {
 
 function moveSnowflakes() {
     for (let i = 0; i < flakes.length; i++) {
-        flakes[i].x += flakes[i].speedX;
-        flakes[i].y += flakes[i].speedY;         
-
-
+        flakes[i].x += flakes[i].speedX - 10;
+        flakes[i].y += flakes[i].speedY ;  
+        
+        if (flakes[i].y > h || flakes[i].x > w || flakes[i].x < 0) {
+            flakes[i].y = - 100;
+            flakes[i].x = Math.random() * w + 100;        
+        }
     }  
 };
 
@@ -56,7 +60,7 @@ function animateSnowflakes() {
     requestAnimationFrame(animateSnowflakes);
 };
 createSnowflakes();
-animateSnowflakes()
+animateSnowflakes();
 
 //setInterval(updateSnowFall, 50); nie uzywa się do animacji (setTimeOut też)
 
