26aba24053129d683c45e44d3f6c8efff45650c2
Tarzanica <patrycja.bobek@microsoft.wsei.edu.pl>
2021-01-12 20:29:40 +0100

    local storage solved

--- lab1/cw3-notepocket/JS/main.js
+++ lab1/cw3-notepocket/JS/main.js
@@ -1,28 +1,28 @@
 const lsNotesKey = 'notes';
-document.querySelector('#newNoteBtn').addEventListener('click', onNewNote);
-const notes = [];
-let convertedNotes;
+document.querySelector('#newNoteBtn').addEventListener('click', createNote);
 const notesContainer = document.querySelector('main');
-if (notes.length == 0) {    
-	document.body.style.backgroundImage = 'url(\'images/no-notes.png\')';
-}
 const pinnedContainer = document.querySelector('.pinned-notes');
+
+let notes = [];
+
+getDataFromLocalStorage();
+displayNotes();
 
 function removeNote(e) {
 	let noteToRemove = e.target.parentNode;
 	noteToRemove.parentNode.removeChild(noteToRemove);
 	notes.splice(noteToRemove, 1);
-
+	localStorage.setItem(lsNotesKey, JSON.stringify(notes));
 }
 
-function onNewNote() {
+function getNoteData() {
 	document.body.style.backgroundImage = 'initial';
 	const title = document.querySelector('#noteTitle').value;
 	const content = document.querySelector('#noteContent').value;
 	const color = document.querySelector('#selected').value;
 	let pinned = false;
 	if((document.querySelector('.pinnedBtn').checked == true)) pinned = true;
-	const note = {
+	let note = {
 		title: title,
 		content: content,
 		colour: color,
@@ -32,77 +32,84 @@ function onNewNote() {
 		
 	notes.push(note);
 	localStorage.setItem(lsNotesKey, JSON.stringify(notes));
-	notesContainer.innerHTML = '';
-	createlocalStorage();
-	createNote();
+	displayNote(note);
 }
-function createlocalStorage(){
 
+function getDataFromLocalStorage(){
+	notes = [];
+	if(localStorage.getItem(lsNotesKey)==null || localStorage.getItem(lsNotesKey)=='null') return;
 	const notesFromLocalStorage = JSON.parse(localStorage.getItem(lsNotesKey));
-	
-	convertedNotes = notesFromLocalStorage.map( note => {
-		note.createDate = new Date(note.createDate);
-		return note;
-	});
+	if (notesFromLocalStorage.length == 0) return;
+	for (let i = 0; i < notesFromLocalStorage.length; i++) {
+		notes.push(notesFromLocalStorage[i]);		
+	}
+
+}
+
+function displayNotes(){
+	for (let i = 0; i < notes.length; i++) {
+		displayNote(notes[i]);		
+	}
 }
 	
-function createNote(){
-	let notes = document.querySelectorAll('.note');
-    
-	for(let i = 0; i < notes.length; i++){
-		notes[i].parentNode.removeChild(notes[i]);
+function displayNote(note){        
+	const htmlNote = document.createElement('section');
+	const htmlTitle = document.createElement('h3');
+	const htmlContent = document.createElement('p');
+	const htmlTime = document.createElement('time');
+	const htmlButton = document.createElement('button');
+	const htmlPinnedBtn = document.createElement ('input');
+	const htmlLabel = document.createElement('label');
+
+	htmlNote.classList.add('note');
+	htmlTitle.innerHTML = note.title;
+	htmlContent.innerHTML = note.content;
+	htmlTime.innerHTML = note.createDate.toLocaleString();
+	htmlButton.innerHTML = 'Remove';
+	htmlButton.classList.add('removeBtn');
+	htmlPinnedBtn.type = 'checkbox';
+	htmlPinnedBtn.id = 'check';
+	htmlPinnedBtn.className = 'inputBig';
+	htmlLabel.htmlFor = 'check';
+
+	if (!htmlNote.style.backgroundColor == 'white') {
+		htmlNote.style.color = 'white';
 	}
-	
-	for (const note of convertedNotes) {
-        
-		const htmlNote = document.createElement('section');
-		const htmlTitle = document.createElement('h3');
-		const htmlContent = document.createElement('p');
-		const htmlTime = document.createElement('time');
-		const htmlButton = document.createElement('button');
-		const htmlPinnedBtn = document.createElement ('input');
-		const htmlLabel = document.createElement('label');
+	else{
+		htmlNote.style.color = 'black';
+	}
 
-		htmlNote.classList.add('note');
-		htmlTitle.innerHTML = note.title;
-		htmlContent.innerHTML = note.content;
-		htmlTime.innerHTML = note.createDate.toLocaleString();
-		htmlButton.innerHTML = 'Remove';
-		htmlButton.classList.add('removeBtn');
-		htmlPinnedBtn.type = 'checkbox';
-		htmlPinnedBtn.id = 'check';
-		htmlPinnedBtn.className = 'inputBig';
-		htmlLabel.htmlFor = 'check';
-
-		htmlNote.style.borderBottomColor = note.colour;
-		htmlPinnedBtn.addEventListener('click', function() {
-			if (htmlNote.parentNode == pinnedContainer) {
-				notesContainer.appendChild(htmlNote);
-			}else if(htmlNote.parentNode == notesContainer){
-				pinnedContainer.appendChild(htmlNote);
-			}			
-		});
-		htmlButton.addEventListener('click', removeNote);
-		htmlNote.appendChild(htmlTitle);
-		htmlNote.appendChild(htmlContent);
-		htmlNote.appendChild(htmlTime);
-		htmlNote.appendChild(htmlButton);
-		htmlNote.appendChild(htmlPinnedBtn);
-		htmlNote.appendChild(htmlLabel);
-
-		if(note.pinned == false){
+	htmlNote.style.backgroundColor = note.colour;
+	htmlPinnedBtn.addEventListener('click', function() {
+		if (htmlNote.parentNode == pinnedContainer) {
 			notesContainer.appendChild(htmlNote);
-			htmlPinnedBtn.checked = false;
-		}
-		else{
+		}else if(htmlNote.parentNode == notesContainer){
 			pinnedContainer.appendChild(htmlNote);
-			htmlPinnedBtn.checked = true;
-		}
+		}			
+	});
+	htmlButton.addEventListener('click', removeNote);
+	htmlNote.appendChild(htmlTitle);
+	htmlNote.appendChild(htmlContent);
+	htmlNote.appendChild(htmlTime);
+	htmlNote.appendChild(htmlButton);
+	htmlNote.appendChild(htmlPinnedBtn);
+	htmlNote.appendChild(htmlLabel);
+
+	if(note.pinned == false){
+		notesContainer.appendChild(htmlNote);
+		htmlPinnedBtn.checked = false;
+	}
+	else{
+		pinnedContainer.appendChild(htmlNote);
+		htmlPinnedBtn.checked = true;
 	}
 
+}
+
+function createNote(){
+	getNoteData();
 	document.querySelector('#noteTitle').value = '';
-	document.querySelector('#noteContent').value = '';
-	
+	document.querySelector('#noteContent').value = '';	
 }
 
 	
